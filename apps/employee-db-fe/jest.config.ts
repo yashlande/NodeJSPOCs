6315f528528a2dcb9295ad17105490c2e5443668
Yashwant <yashlande@gmail.com>
2022-10-16 16:25:08 +0530

    initial commit

--- apps/employee-db-fe/jest.config.ts
+++ apps/employee-db-fe/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'frontend',
+  displayName: 'employee-db-fe',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/frontend',
+  coverageDirectory: '../../coverage/apps/employee-db-fe',
 };
