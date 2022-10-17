9abbc937a92f5c1e81ec6bbd5185764beceb0a8e
Yashwant <yashlande@gmail.com>
2022-10-17 12:36:08 +0530

    UI designing initeated

--- apps/employee-db-fe/src/app/app.tsx
+++ apps/employee-db-fe/src/app/app.tsx
@@ -7,25 +7,6 @@ import { Route, Routes, Link } from 'react-router-dom';
 export function App() {
   return (
     <>
-      <NxWelcome title="employee-db-fe" />
-      <div />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
       <Routes>
         <Route
           path="/"
